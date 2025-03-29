83881b9d18863d485e84f583b3f49dd36e55c447
LilianMallya <92794509+LilianMallya@users.noreply.github.com>
2025-03-29 17:51:37 +0000

    Update Part1Application.java

--- src/main/java/edu/leicester/co2103/Part1Application.java
+++ src/main/java/edu/leicester/co2103/Part1Application.java
@@ -36,30 +36,30 @@ public class Part1Application  implements ApplicationRunner {
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		Session s1 = new Session();
-		s1.setDatetime(Timestamp.valueOf("2022-02-25 12:00:00"));
+		s1.setDatetime(Timestamp.valueOf("2023-01-15 10:00:00"));  
 		s1.setDuration(2);
-		s1.setTopic("REST APIs");
+		s1.setTopic("Web Services Fundamentals");
 		s1 = sessionRepo.save(s1);
-		
-		Module m1 = new Module("CO2103", "Software Architecture", 2, false);
+
+		Module m1 = new Module("CS101", "Intro to Software Systems", 1, false);
 		m1.getSessions().add(s1);
 		m1 = moduleRepo.save(m1);
-				
+        
 		Session s2 = new Session();
-		s2.setDatetime(Timestamp.valueOf("2022-02-28 14:00:00"));
+		s2.setDatetime(Timestamp.valueOf("2023-01-18 13:00:00"));  
 		s2.setDuration(2);
-		s2.setTopic("Unit Testing");
+		s2.setTopic("Testing Principles");
 		s2 = sessionRepo.save(s2);
-		
-		Module m2 = new Module("CO3095", "Software Quality", 3, true);
+
+		Module m2 = new Module("CS201", "Advanced Programming", 2, true);
 		m2.getSessions().add(s2);
 		m2 = moduleRepo.save(m2);
-		
-		Convenor c1 = new Convenor("Jose Rojas", Position.PROFESSOR);
+
+		Convenor c1 = new Convenor("Dr. Smith", Position.PROFESSOR);
 		c1.getModules().add(m1);
 		c1 = convenorRepo.save(c1);
-		
-		Convenor c2 = new Convenor("Kehinde Aruleba", Position.LECTURER);
+
+		Convenor c2 = new Convenor("Dr. Johnson", Position.LECTURER);
 		c2.getModules().add(m1);
 		c2.getModules().add(m2);
 		c2 = convenorRepo.save(c2);
